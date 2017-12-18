32118cb22ce165b676b62cc3f09e61b9c1e93b24
bdlewis30 <32585973+bdlewis30@users.noreply.github.com>
2017-12-18 14:19:35 -0700

    initial commit

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,39 @@
 // change this to true to see test results on the black diamond section.
 export const BLACK_DIAMOND = false;
+
+// TYPES
+const INCREMENT = "INCREMENT"
+const DECREMENT = "DECREMENT"
+
+// ACTION BUILDERS are exported NOT default
+export function increment(amount) {
+    return {
+        type: INCREMENT,
+        payload: amount
+    }
+}
+
+// Action builders always return an object
+export function decrement(amount) {
+    return {
+        type: DECREMENT,
+        payload: amount
+    }
+}
+
+const initialState = {
+    currentValue: 0
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            //1. setup changing data - currentValue
+            let sum = state.currentValue + action.payload;
+            //2. Put changes on new state and return
+            return Object.assign({}, state, { currentValue: sum });
+        case DECREMENT:
+            let differnece = state.currentValue - action.payload;
+            return Object.assign({}, state, { currentValue: differnece })
+    }
+}
